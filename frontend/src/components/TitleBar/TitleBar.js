ea8a611972b2852faddccbb9423a08a2f30f81f4
jon <jon.halquist@gmail.com>
2022-06-09 17:56:00 -0500

    add comment button added to title bar

--- frontend/src/components/TitleBar/TitleBar.js
+++ frontend/src/components/TitleBar/TitleBar.js
@@ -4,16 +4,21 @@ import './TitleBar.css'
 
 const TitleBar = ({ title, button = null}) => {
 
-  const buttonType = () => {
-    return (
-      <div className='editButton'></div>
-    )
+  let buttonType;
+  if (button = 'comment') {
+    buttonType = (
+      <div id='editButton'>Add Comment</div>
+    );
+  } else {
+    buttonType = null
   }
 
 
   return (
     <div className='titleBar'>
+
       <div className='titleText'>{title}</div>
+      {buttonType}
     </div>
   )
 };
